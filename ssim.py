ed21cde019b1b393ec6f7807ccaa9afe405d55a4
“ <965072677@qq.com>
2021-08-09 14:36:40 -0400

    --new version--

--- ssim.py
+++ ssim.py
@@ -2,11 +2,12 @@ from skimage.metrics import structural_similarity
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def ssim():
     ssim = []
-    for i in range(1441):
-        print("=========" + str(i) + "========+")
+    for i in range(1440):
+        print("=========" + str(i) + "=========")
         img1 = Image.open(".\\FruitingBodydata4\\Early\\Early_1\\" + str(i + 1) + ".tif")
         img_arr1 = np.array(img1.getdata()).reshape((1200, 1600))
         img2 = Image.open(".\\FruitingBodydata4\\Early\\Early_1\\" + str(i + 2) + ".tif")
@@ -32,7 +33,12 @@ def psnr():
     return psnr
 
 
-ssim = ssim()
-print(ssim)
-plt.plot(ssim)
+# ssim = ssim()
+# print(ssim)
+# plt.plot(ssim)
+# plt.show()
+
+psnr = psnr()
+print(psnr)
+plt.plot(psnr)
 plt.show()
